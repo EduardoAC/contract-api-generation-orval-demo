1312cbeb074253c236bbf0963c4db1addcf76bf4
Eduardo AC <EduardoAC@users.noreply.github.com>
2025-01-28 10:31:33 +0000

    Setup Swagger Module to generate Open API schema

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Setup swagger document genrator
+  const config = new DocumentBuilder()
+    .setTitle('API example')
+    .setDescription('The example API description')
+    .setVersion('1.0')
+    .addTag('example')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+  SwaggerModule.setup('swagger', app, documentFactory, {
+    jsonDocumentUrl: 'swagger/json',
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
