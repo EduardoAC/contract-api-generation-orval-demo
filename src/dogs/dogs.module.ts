3d477ef883ce1abab834b7709e7123c2e26361c5
Eduardo AC <EduardoAC@users.noreply.github.com>
2025-01-30 16:11:33 +0000

    Clean up unused logic

--- src/dogs/dogs.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { HttpModule } from '@nestjs/axios';
-import { DogsController } from './dogs.controller';
-
-@Module({
-  imports: [HttpModule],
-  controllers: [DogsController],
-})
-export class DogsModule {}
